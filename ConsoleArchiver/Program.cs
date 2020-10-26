b274384d961c77cba752ac6c484273a5488d4dda
Джейхун Ахмедов <lezgin5.5@mail.ru>
2020-10-26 21:54:49 +0300

    d

--- ConsoleArchiver/Program.cs
+++ ConsoleArchiver/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
+
 namespace ConsoleArchiver
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("sdfdsf");
         }
     }
 }
