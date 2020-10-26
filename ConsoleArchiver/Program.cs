e4e30e28a12545485d598a3847c3324be897e495
Джейхун Ахмедов <lezgin5.5@mail.ru>
2020-10-26 21:08:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleArchiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleArchiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
