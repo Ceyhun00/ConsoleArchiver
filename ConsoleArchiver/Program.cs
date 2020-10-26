39dff8e7ae1d5e6d9cb8d65c614f033938f650eb
Джейхун Ахмедов <lezgin5.5@mail.ru>
2020-10-26 22:00:59 +0300

    d

--- ConsoleArchiver/Program.cs
+++ ConsoleArchiver/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleArchiver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wodsrld!");
         }
     }
 }
